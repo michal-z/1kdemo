835c10b3309fb0bbe3e69c2a4342e03bcd2a8793
Michal Ziulek <michalziulek@gmail.com>
2019-05-16 20:22:56 +0200

    qjulia

--- 1kdemo.asm
+++ 1kdemo.asm
@@ -1,33 +1,33 @@
 format MS COFF
 
-extrn '__imp__ExitProcess@4' as w32ExitProcess:dword
-extrn '__imp__CreateWindowExA@48' as w32CreateWindowEx:dword
-extrn '__imp__SetPixelFormat@12' as w32SetPixelFormat:dword
-extrn '__imp__ChoosePixelFormat@8' as w32ChoosePixelFormat:dword
-extrn '__imp__ShowCursor@4' as w32ShowCursor:dword
-extrn '__imp__GetDC@4' as w32GetDC:dword
-extrn '__imp__GetAsyncKeyState@4' as w32GetAsyncKeyState:dword
-extrn '__imp__PeekMessageA@20' as w32PeekMessage:dword
-extrn '__imp__DispatchMessageA@4' as w32DispatchMessage:dword
-extrn '__imp__SetProcessDPIAware@0' as w32SetProcessDPIAware:dword
-extrn '__imp__timeGetTime@0' as w32timeGetTime:dword
-extrn '__imp__timeBeginPeriod@4' as w32timeBeginPeriod:dword
-extrn '__imp__SwapBuffers@4' as w32SwapBuffers:dword
-extrn '__imp__ChangeDisplaySettingsA@8' as w32ChangeDisplaySettings:dword
+extrn '__imp__ExitProcess@4' as ExitProcess:dword
+extrn '__imp__CreateWindowExA@48' as CreateWindowEx:dword
+extrn '__imp__SetPixelFormat@12' as SetPixelFormat:dword
+extrn '__imp__ChoosePixelFormat@8' as ChoosePixelFormat:dword
+extrn '__imp__ShowCursor@4' as ShowCursor:dword
+extrn '__imp__GetDC@4' as GetDC:dword
+extrn '__imp__GetAsyncKeyState@4' as GetAsyncKeyState:dword
+extrn '__imp__PeekMessageA@20' as PeekMessage:dword
+extrn '__imp__DispatchMessageA@4' as DispatchMessage:dword
+extrn '__imp__timeGetTime@0' as timeGetTime:dword
+extrn '__imp__timeBeginPeriod@4' as timeBeginPeriod:dword
+extrn '__imp__SwapBuffers@4' as SwapBuffers:dword
+extrn '__imp__ChangeDisplaySettingsA@8' as ChangeDisplaySettings:dword
 extrn '__imp__wglMakeCurrent@8' as wglMakeCurrent:dword
 extrn '__imp__wglCreateContext@4' as wglCreateContext:dword
 extrn '__imp__wglGetProcAddress@4' as wglGetProcAddress:dword
 extrn '__imp__glRecti@16' as glRecti:dword
 extrn '__imp__glTexCoord1f@4' as glTexCoord1f:dword
+extrn '__imp__SetProcessDPIAware@0' as SetProcessDPIAware:dword
 
 section '.text' code readable executable
 
 public _Start
 _Start:
-        call    [w32SetProcessDPIAware]
         push    00000004h               ; CDS_FULLSCREEN
         push    ScreenSettings
-        call    [w32ChangeDisplaySettings]
+        call    [ChangeDisplaySettings]
+        call    [SetProcessDPIAware]
         push    0                       ; lpParam
         push    0                       ; hInstance
         push    0                       ; hMenu
@@ -38,30 +38,30 @@ _Start:
         push    0                       ; x
         push    91000000h               ; dwStyle = WS_POPUP|WS_VISIBLE|WS_MAXIMIZE
         push    0                       ; lpWindowName
-        push    WinClassName            ; lpClassName
+        push    S.WinClassName          ; lpClassName
         push    0                       ; dwExStyle
-        call    [w32CreateWindowEx]
+        call    [CreateWindowEx]
         push    eax                     ; hwnd
-        call    [w32GetDC]
+        call    [GetDC]
         mov     ebx,eax                 ; ebx = hdc
         push    PixelFormatDesc
         push    ebx                     ; hdc
-        call    [w32ChoosePixelFormat]
+        call    [ChoosePixelFormat]
         push    PixelFormatDesc
         push    eax                     ; pixel format id
         push    ebx                     ; hdc
-        call    [w32SetPixelFormat]
+        call    [SetPixelFormat]
         push    ebx                     ; hdc
         call    [wglCreateContext]
         push    eax                     ; GL context
         push    ebx                     ; hdc
         call    [wglMakeCurrent]
         push    0
-        call    [w32ShowCursor]
-        push    @glUseProgram
+        call    [ShowCursor]
+        push    S.glUseProgram
         call    [wglGetProcAddress]
         mov     esi,eax                 ; esi = pointer to glUseProgram
-        push    @glCreateShaderProgramv
+        push    S.glCreateShaderProgramv
         call    [wglGetProcAddress]
         push    ShaderCodePtr
         push    1
@@ -70,8 +70,8 @@ _Start:
         push    eax                     ; GL program to use
         call    esi                     ; glUseProgram
         push    1                       ; ask for 1 ms timer resolution
-        call    [w32timeBeginPeriod]
-        call    [w32timeGetTime]
+        call    [timeBeginPeriod]
+        call    [timeGetTime]
         mov     edi,eax                 ; edi = beginTime
 .mainLoop:
         push    0001h                   ; PM_REMOVE
@@ -79,8 +79,8 @@ _Start:
         push    0
         push    0
         push    Message
-        call    [w32PeekMessage]
-        call    [w32timeGetTime]
+        call    [PeekMessage]
+        call    [timeGetTime]
         sub     eax,edi                 ; currentTime = time - beginTime
         push    eax
         fild    dword [esp]
@@ -92,14 +92,13 @@ _Start:
         push    -1
         call    [glRecti]
         push    ebx                     ; hdc
-        call    [w32SwapBuffers]
+        call    [SwapBuffers]
         push    1Bh                     ; VK_ESCAPE
-        call    [w32GetAsyncKeyState]
+        call    [GetAsyncKeyState]
         test    eax,eax
         jnz     .exit
         jmp     .mainLoop
-.exit:  push    0
-        call    [w32ExitProcess]
+.exit:  call    [ExitProcess]
         ret
 
 section '.data' data readable writeable
@@ -124,6 +123,6 @@ ShaderCode:
     file '1kdemo-small.glsl'
     db 0
 
-WinClassName db 'edit',0
-@glCreateShaderProgramv db 'glCreateShaderProgramv',0
-@glUseProgram db 'glUseProgram',0
+S.WinClassName db 'edit',0
+S.glCreateShaderProgramv db 'glCreateShaderProgramv',0
+S.glUseProgram db 'glUseProgram',0
